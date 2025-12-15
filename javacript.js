caf037dc599ecc6b1330548c8a17d432ece84a3d
Paul <paularrmekang@yahoo.com>
2025-12-15 15:12:25 -0500

    added javascript

---
+++ javacript.js
@@ -0,0 +1,18 @@
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName);
+console.log(lastName);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+const pi = 3.14;
+console.log(pi);
+
+console.log(23 + 97);
+
+console.log((4 + 6 + 9) / 77);
